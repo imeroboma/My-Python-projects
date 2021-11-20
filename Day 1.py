11bd420786c9589451a1d47c81a8c6b5e6a703f7
imeroboma <84933908+imeroboma@users.noreply.github.com>
2021-11-19 19:32:17 -0700

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+print("Hello" +" "+"Imero");
+print("Day" +" "+ "1" + "String" + " " + "Manipulation")
+print('String Concatention is done with the "+" sign.')
+print('e.g. print("Hello" + "world")')
+print("New lines would be created with a backslash and n.")
+print("Hello" + " " + input("What is your name?"))
+
+
+
+
+
